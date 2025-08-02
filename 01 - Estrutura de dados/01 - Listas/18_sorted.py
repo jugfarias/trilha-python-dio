6a07d6665adeef45d9c9b19bc7cb5d85b11108ba
jugfarias <jufarias13@gmail.com>
2025-08-02 15:54:22 -0300

    comentário sobre parâmetros do sorted

--- 01 - Estrutura de dados/01 - Listas/18_sorted.py
+++ 01 - Estrutura de dados/01 - Listas/18_sorted.py
@@ -1,3 +1,9 @@
+'''
+    sorted é uma função built-in que recebe como parâmentros:
+        1. o iterável
+        2. regra da ordenação - opcional, padrão: do menor ao maior
+'''
+
 linguagens = ["python", "js", "c", "java", "csharp"]
 
 print(sorted(linguagens, key=lambda x: len(x)))  # ["c", "js", "java", "python", "csharp"]
