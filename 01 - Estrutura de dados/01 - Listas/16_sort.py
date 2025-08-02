5de6f8fe3ae2cdc33e8a627ca45a6eec68ddfaff
jugfarias <jufarias13@gmail.com>
2025-08-02 15:51:32 -0300

    comentário sobre argumentos possíveis do .sort()

--- 01 - Estrutura de dados/01 - Listas/16_sort.py
+++ 01 - Estrutura de dados/01 - Listas/16_sort.py
@@ -1,3 +1,11 @@
+'''
+    sort ordena numerica ou alfabeticamente a lista
+    argumentos:
+        reverse = True  # ordena ao contrário
+        key = lambda x: len(x)  # ordena de acordo com o tamanho das palavras (lambda é uma função anônima)
+        key = lambda x: len(x), reverse = True  # ordena de acordo com o tamanho das palavras, ao contrário
+'''
+
 linguagens = ["python", "js", "c", "java", "csharp"]
 linguagens.sort()  # ["c", "csharp", "java", "js", "python"]
 print(linguagens)
