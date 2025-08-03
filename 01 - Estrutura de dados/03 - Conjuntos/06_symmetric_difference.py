0575d05d5382291a244534e6ba0537d119a37194
jugfarias <jufarias13@gmail.com>
2025-08-03 19:31:10 -0300

    add comentário

--- 01 - Estrutura de dados/03 - Conjuntos/06_symmetric_difference.py
+++ 01 - Estrutura de dados/03 - Conjuntos/06_symmetric_difference.py
@@ -1,5 +1,5 @@
 conjunto_a = {1, 2, 3}
 conjunto_b = {2, 3, 4}
 
-resultado = conjunto_a.symmetric_difference(conjunto_b)
+resultado = conjunto_a.symmetric_difference(conjunto_b) # todos os elementos que não estão na intersecção
 print(resultado)
